fd82190ad76727dc280fc411584280fd47ed4731
Markus Bader <markus.bader@mx-robotics.com>
2020-11-04 16:47:09 +0100

    xx

--- common/include/car/motion/motion.h
+++ common/include/car/motion/motion.h
@@ -12,22 +12,27 @@ namespace car
     {
         class MotionVW
         {
-            public : 
-            class State
-            {
-            public:
-                float x[2]; // v, w
-                float y[3]; // power left, power right, angle steering
-                float z[2]; // speed left, speed right
-            };
-            
+                  
             public : MotionVW();
+            static const int V = 0;
+            static const int W = 1;
+            static const int LEFT = 0;
+            static const int RIGHT = 1;
+            static const int STEERING = 2;
+
             void init_mechanic(float *wheel_distance, float *axis_distance, float *wheel_diameter);
             void init_target(float *v, float *w);
-            void init_output(float *motor_power_left, float *motor_power_right, float *steering_angle_front);
-            void init_input(float *motor_speed_left, float *motor_speed_right);
+            void init_controls(float *motor_power_left, float *motor_power_right, float *steering_angle_front);
+            void init_mesaurments(float *motor_speed_left, float *motor_speed_right);
+
+            void update(){
+                
+            }
 
         private:
+            float x[2]; // state v, w
+            float y[3]; // power left, power right, angle steering
+            float z[2]; // speed left, speed right
         };
 
     } // namespace motion
